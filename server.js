ddfcbf28ba047d72b05140b411582a636315999e
Davi Guilherme <job.davic@gmail.com>
2021-12-21 15:35:43 -0300

    a

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname,'/cliente')));
 
 app.post('/recebedados',(req,res)=>{
-    res.send(`Os dados preenchidos foram: Nome: ${req.body.name} - E-mail: ${req.body.email} - Telefone: ${req.body.tel}`);
+   res.send(`Os dados preenchidos foram: Nome: ${req.body.name} - E-mail: ${req.body.email} - Telefone: ${req.body.tel}`);
 })
 
 app.listen(5000,()=>{
-    console.log('Server rodando na porta 5000')
+    console.log('Server rodando na porta 5000');
 })
